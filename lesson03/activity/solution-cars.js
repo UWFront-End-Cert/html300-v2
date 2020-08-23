045f18472aaf188226e25fc913cdb1661548a62a
CheriMarie <cherimarie@gmail.com>
2020-08-23 10:20:20 -0700

    WIP

--- lesson03/activity/solution-cars.js
+++ lesson03/activity/solution-cars.js
@@ -1,4 +1,4 @@
-const carsJSON = "[{\"make\":\"Ford\",\"model\":\"Mustang\",\"year\":2010,\"color\":\"black\"},{\"make\":\"Chevy\",\"model\":\"Corvette\",\"year\":1984,\"color\":\"red\"},{\"make\":\"Jeep\",\"model\":\"Wrangler\",\"year\":1999,\"color\":\"silver\"}]"
+const carsJSON = '[{"make":"Ford","model":"Mustang","year":2010,"color":"black"},{"make":"Chevy","model":"Corvette","year":1984,"color":"red"},{"make":"Jeep","model":"Wrangler","year":1999,"color":"silver"}]'
 const cars = JSON.parse(carsJSON)
 
 const container = document.querySelector('#list-container')
