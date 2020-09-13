826772d5f88d26ab928b7405f67a453b4f4cf178
swati <swatidash2007@gmail.com>
2020-09-13 16:08:33 -0700

    added some js functionality

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -40,6 +40,7 @@ const data = [5, 4, 8, 9, 3],
      const som = data.map(function(n) {
      //here num is the user input number
      // n is each element of the array
+     //here the map method takes the input value, multiplies it to each elements of an array and returns a new array
                   return num * n;
 
 
@@ -47,6 +48,87 @@ const data = [5, 4, 8, 9, 3],
         //console.log(num);
       result.text(som);
       input.val('');
- 
+
 });
+});
+
+
+//filter method()
+$(function () {
+const array = [4, 8, 6, 7, 3, 11, 16];
+
+$(".submit1").on("click", function() {
+  //get user input value
+  const inputVal = $(".input1").val();
+  //validate inputvalue to numbers using parseInt
+  const inputVal1 = parseInt(inputVal);
+// use filter function to differentiate numbers which has zero remainder aftervdevided by 2(even numbers in array)
+  let arr2 = array.filter(function(num) {
+  return num % 2 == 0;
+  })
+//show array of even numbers on browser
+  $(".result1").text(arr2);
+
+  $(".input1").val("");
+
+});
+
+
+
+});
+
+
+//reduce() method
+
+$(function () {
+  //set the array
+const arr = [1,2,3, 10];
+$(".submit2").on("click", function(n) {
+//use reduce method to sum up the array value
+let arr2 = arr.reduce(function(sum, val) {
+            return sum + val;
+   });
+   console.log(arr2);
+   //set a variable to get value
+   const num = $(".input2").val();
+   //validate input value
+   const num2 = parseInt(num);
+
+console.log(num2);
+//first the reduce method summed up the array elements and then multiplies with the input value and show it on browser
+$(".showOutput2").text(num2 * arr2);
+
+
+
+});
+
+});
+
+
+//using some() method
+
+$(function () {
+//setting an array
+const myArr = [24, 32, 44];
+//const myArr = [];
+$(".submit3").on("click", function() {
+//set variables for input and converting input value to valid numbers using parseInt
+ const inputUser = $(".input3").val();
+ const inputFinal = parseInt(inputUser);
+ //using some() method to check if the input value is greater than 10
+ //show true if its more than 10 and show false if its less than 10
+ let arr2 = myArr.some(function() {
+    return inputFinal > 10;
+ });
+ console.log(arr2);
+
+//console.log(arr2);
+
+$(".showOutput3").text(arr2);
+//empty the user input box after the execution
+$(".input3").val("");
+
+
+})
+
 });
