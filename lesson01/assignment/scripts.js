0f66eb3b516c0e5d09ac19a957378e1c3f30c712
cheri allen <cherimarie@users.noreply.github.com>
2020-11-10 17:04:06 -0800

    Revamp the example code for assignment (#11)

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -1,26 +1,33 @@
-// jQuery Doc Ready
-$(function () {
-    // Set up some data and variables
-    const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultSum = $('.sum');
-    // Click event on the submit input
-    submit.on('click', function () {
-        // Grab the input value and parse it into a number
-        const val = input.val();
-        const num = parseInt(val);
-        // Verify the user gave us a real number
-        if (!isNaN(num)) {
-            // Add our new number to the data array
-            data.push(num);
-            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-            const sum = data.reduce((acc, curr) => acc + curr);
-            // Add our new total to the page
-            resultSum.text(sum);
-            // Reset the input value to an empty string
-            input.val('');
-        }
-    });
-});
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
 
+  // Find the example form, and handle its submit event
+  const exampleForm = document.querySelector('#example-form')
+
+  exampleForm.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    let data = [0, 1, 2, 3, 5, 8]
+
+    const inputField = document.querySelector('#example-form .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    // If the user's input was a number, push it into the array and continue
+    if(!isNaN(userNumber)){
+      data.push(userNumber)
+
+      // Use reduce to sum all the numbers in the data array
+      const sum = data.reduce((accumulator, currentElement) => accumulator + currentElement);
+
+      // Output the total to the page- no need to convert the numeric sum back to a string, HTML will just print the number as a string
+      document.querySelector('#example-form .output').innerHTML = sum
+    }
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+
+})
