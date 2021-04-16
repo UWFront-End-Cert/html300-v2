7f888a8bc28a9bfe3d25afc6d6d390c16305e44c
Victor Hoang <vthoang@msn.com>
2021-04-15 18:14:40 -0700

    first commit

---
+++ lesson01/InClass Notes/index.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector("img").getAttribute("alt"))
+
+//slide 19
+//document.getElementsByTagName("img")
+//console.log(document.getElementsByTagName("div"))
+//document.querySelectorAll("[class^=interactive-]")
+
+//slide 21
+//document.querySelector("img").getAttribute("alt")
+//document.getElementById('mega-menu').classList
+//document.getElementsByClassName("event__body")[2].textContent
--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -29,5 +29,122 @@ document. addEventListener("DOMContentLoaded", function(){
     // Clear the form field so the user can try again
     inputField.value = ''
   })
-
 })
+
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the example form, and handle its submit event
+  const form1 = document.querySelector('#form1')
+
+  form1.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    const people = [
+    {name: 'Victor', age: 30},
+    {name: 'Gail', age: 28},
+    {name: 'John', age: 31},
+    {name: 'Nhi', age: 60},
+    {name: 'Daniel', age: 25},
+    {name: 'Du', age: 70}
+    ];
+
+    const inputField = document.querySelector('#form1 .input');
+    const userInput = inputField.value;
+    const person = people.find(({name}) => name === userInput);
+ 
+    // output the array item quantity based on user input
+    document.querySelector('#form1 .output').innerHTML = person.age;
+    document.querySelector('#form1 .name').innerHTML = userInput;
+ 
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the example form, and handle its submit event
+  const form2 = document.querySelector('#form2')
+
+  form2.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    const cpuParts = ['CPU', 'RAM', 'GPU', 'PSU', 'motherboard', 'case', 'SSD', 'wireless card', 'fans', 'OS', 'operating system', 'hard drive']
+
+    const inputField = document.querySelector('#form2 .input');
+    const userInput = inputField.value;
+ 
+    // output the array item quantity based on user input
+    document.querySelector('#form2 .output').innerHTML = cpuParts.includes(userInput);
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the example form, and handle its submit event
+  const form3 = document.querySelector('#form3')
+
+  form3.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    const people = [
+    {name: 'Victor', age: 30},
+    {name: 'Gail', age: 28},
+    {name: 'John', age: 31},
+    {name: 'Nhi', age: 60},
+    {name: 'Daniel', age: 25},
+    {name: 'Du', age: 70}
+    ];
+    const ages = [30, 28, 31, 60, 25, 70]
+
+    const inputField = document.querySelector('#form3 .input');
+    const userInput = inputField.value;
+    const ageMultiplied = ages.map(x => x * userInput);
+ 
+    // output the array item quantity based on user input
+    document.querySelector('#form3 .output').innerHTML = ageMultiplied;
+ 
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the example form, and handle its submit event
+  const form4 = document.querySelector('#form4')
+
+  form4.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    const phoneNumber = [4252758825, 2062421637, 4253799586];
+
+    const inputField = document.querySelector('#form4 .input');
+    const userInput = inputField.value;
+    const userNumber = parseInt(userInput)
+
+    if(!isNaN(userNumber)){
+      phoneNumber.push(userNumber)
+
+    const shortPhoneNumbers = phoneNumber.every(n => n.length < 99999999999);
+    
+ 
+    // output the array item quantity based on user input
+    document.querySelector('#form4 .output').innerHTML = phoneNumber;
+    document.querySelector('#form4 .output2').innerHTML = shortPhoneNumbers;
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  }
+  })
+})
+
