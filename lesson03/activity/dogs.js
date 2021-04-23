576617ee11fc7096580ae9034cbf02cf1af08f12
Victor Hoang <vthoang@msn.com>
2021-04-22 19:17:24 -0700

    in class activity with Yuting and Juan

--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,5 +1,19 @@
 // Create the Dog class constructor
+function dog(name, age, breed, color, barkLevel, energyLevel) {
+    this.name = name,
+    this.age = age,
+    this.breed = breed,
+    this.color = color,
+    this.barkLevel = "10",
+    this.energyLevel = "0"
 
+    function pat(barkLevel, energyLevel){
+        this.barkLevel = this.barkLevel - 1;
+        this.energyLevel = this.energyLevel + 1;
+        console.log(this.name, this.age, this.barkLevel, this.energyLevel);
+    };
+}
 // Create Marty
-
+const Marty = new dog('Marty', 1, 'beagle', 'white', 5, 3);
+console.log(Marty);
 // Pat him 5 times
