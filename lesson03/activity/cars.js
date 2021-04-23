78899a217006c5009c78bb8f8305ca816b355e5c
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-04-23 11:48:32 +0800

    in class practice

--- lesson03/activity/cars.js
+++ lesson03/activity/cars.js
@@ -1,3 +1,13 @@
 // TODO: Create HTML code from cars JSON
 
 const carsJSON = '[{"make":"Ford","model":"Mustang","year":2010,"color":"black"},{"make":"Chevy","model":"Corvette","year":1984,"color":"red"},{"make":"Jeep","model":"Wrangler","year":1999,"color":"silver"}]'
+
+const cars = JSON.parse(carsJSON)
+
+console.log(cars[0])
+
+let output = "hello!"
+
+//TODO  make output variable hold html with info about each car in the list
+
+document.querySelector("#list-container").innerHTML = output
