55b5d0843c61ab1264b8b34425ecbf58965ba1d7
Victor Hoang <vthoang@msn.com>
2021-04-22 20:54:19 -0700

    in class activities and homework to add index.js

--- lesson03/activity/cars.js
+++ lesson03/activity/cars.js
@@ -1,3 +1,19 @@
 // TODO: Create HTML code from cars JSON
 
 const carsJSON = '[{"make":"Ford","model":"Mustang","year":2010,"color":"black"},{"make":"Chevy","model":"Corvette","year":1984,"color":"red"},{"make":"Jeep","model":"Wrangler","year":1999,"color":"silver"}]'
+
+const cars = JSON.parse(carsJSON)
+console.log(cars[0])
+
+let output = "<ul>"
+
+//TODO make output variable hold html with info about each car in the list
+//this.make, this.model, this.year
+cars.forEach(function(car){
+    //print out key and value
+    output += `<p>Your car is a: </p><li>${car.make}</li>` //or output = output + `<li>${car.make}</li>`
+})
+
+output += "</ul>"
+
+document.querySelector("#list-container").innerHTML = output
--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,19 +1,23 @@
 // Create the Dog class constructor
-function dog(name, age, breed, color, barkLevel, energyLevel) {
-    this.name = name,
-    this.age = age,
-    this.breed = breed,
-    this.color = color,
-    this.barkLevel = "10",
-    this.energyLevel = "0"
-
-    function pat(barkLevel, energyLevel){
-        this.barkLevel = this.barkLevel - 1;
-        this.energyLevel = this.energyLevel + 1;
-        console.log(this.name, this.age, this.barkLevel, this.energyLevel);
-    };
+function dog(name, age, breed, color) {
+    this.name = name
+    this.age = age
+    this.breed = breed
+    this.color = color
+    this.barkLevel = 10
+    this.energyLevel = 0
+    this.pat = function(){
+        this.energyLevel += 1 //or ++ //or this.energyLevel + 1
+        this.barkLevel -= 1
+        console.log(`This dog is called ${this.name} and is ${this.age * 7} human years old. Bark level is ${this.barkLevel}, energy level is ${this.energyLevel}.`);
+    }
 }
 // Create Marty
-const Marty = new dog('Marty', 1, 'beagle', 'white', 5, 3);
+const Marty = new dog('Marty', 1, 'beagle', 'white');
 console.log(Marty);
 // Pat him 5 times
+Marty.pat()
+Marty.pat()
+Marty.pat()
+Marty.pat()
+Marty.pat()
