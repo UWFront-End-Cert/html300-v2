a505773fcd747f9d7774c0893fab6014262766d0
Suzanne DePoe <suzannedepoe@Suzannes-MacBook-Air.local>
2021-04-26 21:31:18 -0700

    updated files with changes +2 cards

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,3 +1,4 @@
+const cardList = 
 [
   {
     "name": "Paolo Maldini",
@@ -52,3 +53,81 @@
     ]
   }
  ]
+
+ for (let i = 0; i < cardList.length; i++) {
+   const cards = cardList[i];
+
+   const cardContainer = document.getElementById('container-list');
+   console.log(cardContainer);
+   console.log(cardList[i]);
+
+   const whoFrame = document.createElement('div');
+   whoFrame.setAttribute('class', 'card-who');
+   cardContainer.append(whoFrame);
+
+   const cardImage = document.createElement('img');
+   cardImage.setAttribute('src', cards.img);
+   cardImage.setAttribute('alt', cards.alt);
+   cardImage.setAttribute('class', 'pup')
+   whoFrame.append(cardImage);
+
+   const cardName = document.createElement('h3');
+   cardName.setAttribute('class', 'card-name');
+   cardImage.append(cardName);
+
+   const jobName = document.createElement('p');
+   jobName.setAttribute('class', 'job');
+   cardName.append(jobName);
+
+   const jobTitle  = document.createElement('div');
+   jobTitle.setAttribute('class', 'info');
+   jobName.append(jobTitle);
+
+   const listItems = document.createElement('ul');
+   listItems.setAttribute('id', 'listItems');
+   jobTitle.append(listItems);
+
+   const company = document.createElement('li');
+   listItems.append(company);
+   
+   const experience = document.createElement('li');
+   company.append(experience);
+   
+   const school = document.createElement('li');
+   experience.append(school);
+   
+   const major = document.createElement('li');
+   school.append(major);
+   
+   const email = document.createElement('li');
+   major.append(email);
+
+   const languages = document.createElement('li');
+   email.append(languages);
+
+   const profile = document.createElement('div');
+   profile.setAttribute('class', 'linked');
+   languages.append(profile);
+
+   const linkedIcon = document.createElement('img');
+   linkedIcon.setAttribute('src', cards.img);
+   linkedIcon.setAttribute('alt', cards.alt);
+   profile.append(linkedIcon);
+
+   const linkedProfile = document.createElement('a');
+   linkedIcon.append(linkedProfile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
