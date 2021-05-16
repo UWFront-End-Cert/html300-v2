be1b88fa42cf2f9c59890c2b4c659c78b1c9a963
Cahillmn <mcahill@seattleu.edu>
2021-05-16 15:10:06 -0700

    finished lesson 6

--- lesson06/assignment/index.js
+++ lesson06/assignment/index.js
@@ -3,14 +3,14 @@ function ValidateEmail(inputText)
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(inputText.value.match(mailformat))
 {
-document.form1.text1.focus();
+document.form1.text2.focus();
 alert("Thank You");
 return true;
 }
 else
 {
 alert("You have entered an invalid email address!");
-document.form1.text1.focus();
+document.form1.text2.focus();
 return false;
 }
 }
