a8fd63f0d062a5bff81dadc6db06894172533cc1
unknown <maria.p.k@hotmail.com>
2021-08-22 11:02:36 -0700

    modified html and js

--- L1-Assignment/es6-assignment1.js
+++ L1-Assignment/es6-assignment1.js
@@ -1,6 +1,12 @@
 //declare array
 let myArray = [10, 20, 30, 40, 50]
 
+//declare literals
+let pushString = ' has been added to the existing array:  '
+let mapString = ' has been added to each value of the existing array and the new values for the new array are:  '
+let filterString = 'The following values are greater than 30:  '
+let reduceString = 'The sum of the values entered for the array has been reduced to a single number:  '
+
 //access form1
 const form1 = document.querySelector('#pushToArray')
 
@@ -19,14 +25,15 @@ form1.addEventListener('submit', function(event) {
     }      
 
     //add a node to the document and display the output in that element
-    const text1 = document.createTextNode(userNumber1 + ' has been added to the existing array:  ' + myArray)
-    const h1 = document.createElement('h4')
-    h1.appendChild(text1)
-    this.appendChild(h1)
+    const text = document.createTextNode(`The value ${userNumber1} ${pushString} ${myArray}`)
+    const h = document.createElement('h4')
+    h.appendChild(text)
+    this.appendChild(h)
     
     this.classList.add('push')
 
 })
+
 
 //access form2
 const form2 = document.querySelector('#mapToArray')
@@ -40,14 +47,14 @@ const userInput2 = document.querySelector('#mapToArray .input2').value
 const userNumber2 = parseInt(userInput2)
 
 //map user input number to existing array and return a new array with new values
-let myArray2 = myArray.map(function(value){
+let myArray2 = myArray.map(value => {
 return value + userNumber2
 })
 
 //create a header element and display the output in that element
-const text2 = document.createTextNode(userNumber2 + ' has been added to each value of the existing array and the new values for the new array are:  ' + myArray2)
+const text = document.createTextNode(`The value ${userNumber2} ${mapString}${myArray2}`)
     const h = document.createElement('h4')
-    h.appendChild(text2)
+    h.appendChild(text)
     this.appendChild(h)
     
     this.classList.add('mapping')
@@ -61,17 +68,33 @@ const form3 = document.querySelector('#filterArray')
 form3.addEventListener('submit', function(event) {
     event.preventDefault()
 
+//declare an empty array
+let inputArray = []
+
+//Take user input values and add to inputArray
+   const i1 = document.querySelector('#filterArray .val1').value
+   const userInput1 = parseInt(i1)
+   inputArray.push(userInput1)
+   
+  const i2 = document.querySelector('#filterArray .val2').value
+  const userInput2 = parseInt(i2)
+  inputArray.push(userInput2)
+   
+  const i3 = document.querySelector('#filterArray .val3').value
+  const userInput3 = parseInt(i3)
+  inputArray.push(userInput3)
+
     //retrieve all the values in the array that are > 30
-    let outputField = myArray.filter(function(value){
+    let outputField = inputArray.filter(value => {
     
         return value > 30
     })
 
     //create a header element and display the filtered values
-    const text3 = document.createTextNode('The array contains the following values > 30:  ' + outputField)
-    const h3 = document.createElement('h4')
-    h3.appendChild(text3)
-    this.appendChild(h3)
+    const text = document.createTextNode(`${filterString}${outputField}`)
+    const h = document.createElement('h4')
+    h.appendChild(text)
+    this.appendChild(h)
     this.classList.add('filter')
 
 })
@@ -84,8 +107,11 @@ const form4 = document.querySelector('#reduce')
 form4.addEventListener('submit', function(event) {
     event.preventDefault()
 
+    
+//declare an empty array
 let inputArray = []
 
+//Take user input values and add to inputArray
    const i1 = document.querySelector('#reduce .val1').value
    const userInput1 = parseInt(i1)
    inputArray.push(userInput1)
@@ -99,20 +125,16 @@ let inputArray = []
   inputArray.push(userInput3)
 
 let sum = 0
-let sum2 = inputArray.reduce(function(total, value){
-return total + value
-})
-
-console.log(inputArray)
-console.log(sum2)
+// let sum2 = inputArray.reduce(function(total, value){
+    let sum2 = inputArray.reduce((total, value) => total + value)
 
 //create a header element and display the filtered values
- const text4 = document.createTextNode('The sum of the values entered for the array is:  ' + sum2)
+ const text = document.createTextNode(`${reduceString}${sum2}`)
  
- const h4 = document.createElement('h4')
- h4.appendChild(text4)
- this.appendChild(h4)
-  this.classList.add('reduce')
+ const h = document.createElement('h4')
+ h.appendChild(text)
+ this.appendChild(h)
+ this.classList.add('reduce')
 
 })
 
