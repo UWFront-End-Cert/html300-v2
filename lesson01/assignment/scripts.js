e6adc109d400648584e154307a121f08f5f2de81
Waruggy <cwang8413@yahoo.com>
2021-09-28 04:43:43 -0700

    Have added my ES6 methods and put four functions

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -1,30 +1,122 @@
 // Code that requires the DOM be loaded should not be run until the DOM is loaded
 document. addEventListener("DOMContentLoaded", function(){
 
-  // Find the example form, and handle its submit event
-  const exampleForm = document.querySelector('#example-form')
+  // Find the function-1 form, and handle its submit event
+  const exampleForm = document.getElementById('function-1')
 
   exampleForm.addEventListener('submit', function(event){
     // Prevent the form from trying to submit to a server
     event.preventDefault()
 
     // An array that we can transform based on user input
-    let data = [0, 1, 2, 3, 5, 8]
+    let data = [ 1, 2, 3, 5, 24]
 
-    const inputField = document.querySelector('#example-form .input')
+    const inputField = document.querySelector('#function-1 .input')
     const userInput = inputField.value
     const userNumber = parseInt(userInput)
 
-    // If the user's input was a number, push it into the array and continue
-    if(!isNaN(userNumber)){
-      data.push(userNumber)
+    // Perform a mod of each number in data array with user input and return result
+    const result = data.map( x =>  x % userNumber);
 
-      // Use reduce to sum all the numbers in the data array
-      const sum = data.reduce((accumulator, currentElement) => accumulator + currentElement);
+    // Set inner html text to manipulated array
+    document.querySelector('#function-1 .output').innerHTML = result.toString();
 
-      // Output the total to the page- no need to convert the numeric sum back to a string, HTML will just print the number as a string
-      document.querySelector('#example-form .output').innerHTML = sum
+    // Console log the manipualted array
+    console.log(`The result of this calculation is ${result}`);
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+
+})
+
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the function-2 form, and handle its submit event
+  const exampleForm = document.getElementById('function-2')
+
+  exampleForm.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    let data = [ 1, 2, 3, 5, 24]
+
+    const inputField = document.querySelector('#function-2 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    // Perform a mod of each number in data array with user input and return result
+    const result = data.map( x =>  x > userNumber);
+
+    // If array is empty set inner HTML to message, else set to array
+    if (result.length == 0)
+    {
+      document.querySelector('#function-2 .output').innerHTML = "My array had no values greater than your input!";
     }
+    else
+    {
+      document.querySelector('#function-2 .output').innerHTML = result.toString();
+    }
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+
+})
+
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the function-3 form, and handle its submit event
+  const exampleForm = document.getElementById('function-3')
+
+  exampleForm.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    let data = [ 1, 2, 3, 5, 24]
+
+    const inputField = document.querySelector('#function-3 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    // Tests whether atleast one number in the array is divisible by user input
+    const result = data.some( x =>  x % userNumber === 0);
+
+    // Set inner html to array
+    document.querySelector('#function-3 .output').innerHTML = result;
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+
+})
+
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the function-4 form, and handle its submit event
+  const exampleForm = document.getElementById('function-4')
+
+  exampleForm.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array that we can transform based on user input
+    let data = [1, 2, 3, 5, 24]
+
+    const inputField = document.querySelector('#function-4 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    // Sum all values in array and multiply by user input
+   const result = data.reduce((prev, next) => prev + next)*userNumber;
+
+   // Set inner html to array
+   document.querySelector('#function-4 .output').innerHTML = result;
 
     // Clear the form field so the user can try again
     inputField.value = ''
