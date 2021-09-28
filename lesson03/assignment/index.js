36d9f7c3281b08ea3e6e88cd5cf9f86d7fa57391
matthew <leematt5@uw.edu>
2021-09-27 21:57:34 -0700

    converted code to use string literals

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -3,7 +3,7 @@
 // Code that requires the DOM be loaded should not be run until the DOM is loaded
 document. addEventListener("DOMContentLoaded", function(){
 
-const userData = [
+const userData = `[
   {
     "name": "Paolo Maldini",
     "jobTitle": "Front End Developer",
@@ -56,217 +56,59 @@ const userData = [
        "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
     ]
   }
- ];
+ ]`
 
 
-userData.map(function(item) {
+// parse json data
+const content = JSON.parse(userData);
 
-const container = document.querySelector(".template-hook");
-container.classList.add('main');
-let employee_card = document.createElement('div');
-employee_card.className = 'employee-card';
-let profile = document.createElement('div');
-profile.className = 'profile';
-let  experience = document.createElement('div');
+// 
+const container  = document.querySelector('.template-hook')
+container.className='main';
 
-experience.className = 'experience';
+content.map(function(item) {
 
-container.appendChild(employee_card);
-employee_card.appendChild(profile);
-employee_card.appendChild(experience);
+  let inside = document.createElement('div');
+  inside.innerHTML = `
+  <div class="employee-card">
+  <div class="profile">
+      <!-- dog -->
+      <img class="headshot" src="./img/headshot.jpg" alt="Paulo's face"/>
+      <p class="title">
+          <strong>${item.name}</strong>
+      </p>
+      <p> 
+          <i>
+          ${item.jobTitle}
+          </i>
+      </p>
+  </div>
+  <div class="experience">
+      <!-- description -->
+      <p>
+          <strong> Company: </strong> ${item.company}
+      </p>
+      <p>
+          <strong> Experience: </strong> ${item.experience}
+      </p>
+      <p>
+          <strong> School: </strong> ${item.school}
+      </p>
+      <p>
+          <strong> Major: </strong> ${item.major}
+      </p>
+      <div class="center">
+          <img src="./img/linkedin.svg" class="logo" alt="linkedin logo">
+          <p class="profile-text">
+              </strong> ${item.linkedInUrl}
+          </p>
+      </div>
+  </div>
+</div> <br>`
 
-let face = document.createElement('img');
-face.src="./img/headshot.jpg";
-face.className='headshot';
-face.alt= item.name;
-profile.appendChild(face);
-
-
-let p1 = document.createElement('p');
-p1.className ='title';
-
-let title = document.createElement('strong');
-title.innerText=item.name;
-p1.appendChild(title);
-
-profile.appendChild(p1);
-
-let name = document.createElement('i');
-name.innerText = item.jobTitle;
-
-let p0 = document.createElement('p');
-
-profile.appendChild(p0);
-p0.appendChild(name);
-
-
-
-let p2 = document.createElement('p');
-
-
-profile.appendChild(p2);
-
-let p3 = document.createElement('p');
-let p4 = document.createElement('p');
-let p5 = document.createElement('p');
-let p6 = document.createElement('p');
-
-p3.innerText = 'Company: ';
-p4.innerText = 'Experience: ';
-p5.innerText = 'School: ';
-p6.innerText = 'Major: ';
-
-let t1 = document.createElement('strong');
-let t2 = document.createElement('strong');
-let t3 = document.createElement('strong');
-let t4 = document.createElement('strong');
-
-
-p3.appendChild(t1);
-p4.appendChild(t2);
-p5.appendChild(t3);
-p6.appendChild(t4);
-
-t1.innerText = item.company;
-
-t2.innerText = item.experience;
-
-t3.innerText = item.school;
-
-t4.innerText = item.major;
-
-experience.appendChild(p3);
-experience.appendChild(p4);
-experience.appendChild(p5);
-experience.appendChild(p6);
-
-let logo = document.createElement('div');
-let logo_image = document.createElement('img');
-let p7 = document.createElement('div');
-
-logo.appendChild(logo_image);
-logo.appendChild(p7);
-
-logo.className = 'center';
-logo_image.src='./img/linkedin.svg';
-logo_image.className='logo';
-logo_image.alt='linkedin logo';
-p7.className='profile-text';
-p7.innerText='paolo.linkedinprofile.com';
-
-experience.appendChild(logo);
-
-let seperate = document.createElement('br');
-container.appendChild(seperate);
+container.appendChild(inside);
 
 })
 
-
-
-
-// const container = document.querySelector(".template-hook");
-// container.classList.add('main');
-// let employee_card = document.createElement('div');
-// employee_card.className = 'employee-card';
-// let profile = document.createElement('div');
-// profile.className = 'profile';
-// let  experience = document.createElement('div');
-
-// experience.className = 'experience';
-
-// container.appendChild(employee_card);
-// employee_card.appendChild(profile);
-// employee_card.appendChild(experience);
-
-// let face = document.createElement('img');
-// face.src="./img/headshot.jpg";
-// face.className='headshot';
-// face.alt='Paulo face';
-// profile.appendChild(face);
-
-
-// let p1 = document.createElement('p');
-// p1.className ='title';
-
-// let title = document.createElement('strong');
-// title.innerText='Paolo Dogo';
-// p1.appendChild(title);
-
-// profile.appendChild(p1);
-
-// let name = document.createElement('i');
-// name.innerText = 'Front End Developer';
-
-// let p0 = document.createElement('p');
-
-// profile.appendChild(p0);
-// p0.appendChild(name);
-
-
-
-// let p2 = document.createElement('p');
-
-
-// profile.appendChild(p2);
-
-// let p3 = document.createElement('p');
-// let p4 = document.createElement('p');
-// let p5 = document.createElement('p');
-// let p6 = document.createElement('p');
-
-// p3.innerText ='Company:';
-// p4.innerText = 'Experience:';
-// p5.innerText = 'School:';
-// p6.innerText = 'Major:';
-
-// let t1 = document.createElement('strong');
-// let t2 = document.createElement('strong');
-// let t3 = document.createElement('strong');
-// let t4 = document.createElement('strong');
-
-
-// p3.appendChild(t1);
-// p4.appendChild(t2);
-// p5.appendChild(t3);
-// p6.appendChild(t4);
-
-// t1.innerText = 'Web Weavers';
-
-// t2.innerText = '3 Years';
-
-// t3.innerText = 'University of Washington';
-
-// t4.innerText = 'Marketing';
-
-// experience.appendChild(p3);
-// experience.appendChild(p4);
-// experience.appendChild(p5);
-// experience.appendChild(p6);
-
-// let logo = document.createElement('div');
-// let logo_image = document.createElement('img');
-// let p7 = document.createElement('div');
-
-// logo.appendChild(logo_image);
-// logo.appendChild(p7);
-
-// logo.className = 'center';
-// logo_image.src='./img/linkedin.svg';
-// logo_image.className='logo';
-// logo_image.alt='linkedin logo';
-// p7.className='profile-text';
-// p7.innerText='paolo.linkedinprofile.com';
-
-// experience.appendChild(logo);
-
-
-// userData.map(function(el){
-
-// let list = document.createElement('ul');
-// list.innerHTML = `<li> ${el.name} </li>`
-
-// container.append(list);
-
-// })
 
 })
