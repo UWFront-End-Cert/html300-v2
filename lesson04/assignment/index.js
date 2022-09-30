351097beb6c32b53bc677ba8e82a075a04b86c8a
deaddiana <deaddian@uw.edu>
2022-09-29 21:48:44 -0700

    test commit

---
+++ lesson04/assignment/index.js
@@ -0,0 +1,25 @@
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+/* Nav barToggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
