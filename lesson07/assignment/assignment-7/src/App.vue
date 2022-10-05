193ee53c8a076a226df65457d869d23d6c510a45
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-10-05 01:43:20 -0700

    Initial assignment-7 files

--- lesson07/assignment/assignment-7/src/App.vue
+++ lesson07/assignment/assignment-7/src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import 'semantic-ui-css/semantic.css'
-// import StaffDirectory from './components/StaffDirectory.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  // components: {
-    // StaffDirectory
-  // }
-};
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  body {
-    padding: 2em;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
