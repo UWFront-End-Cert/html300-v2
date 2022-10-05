f56b566e9690ee3fd9626b645488c50fa708f2b7
deaddiana <deaddian@uw.edu>
2022-10-04 23:31:25 -0700

    completed lesson 4

--- lesson04/assignment/index.js
+++ lesson04/assignment/index.js
@@ -15,11 +15,11 @@ function carousel() {
 
 
 /* Nav barToggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  let x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function myFunction() {
+  //let x = document.getElementById("myTopnav");
+//  if (x.className === "topnav") {
+//    x.className += " responsive";
+///  } else {
+//    x.className = "topnav";
+//  }
+//}
