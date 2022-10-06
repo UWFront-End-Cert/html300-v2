4f6e8565331b0fd3ef40cf603459fbb5911fb0d0
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-10-05 18:54:30 -0700

    Updates to routes

--- lesson07/assignment/assignment-7/src/App.vue
+++ lesson07/assignment/assignment-7/src/App.vue
@@ -6,12 +6,11 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+
+    }
   }
 }
 </script>
--- lesson07/assignment/assignment-7/src/main.js
+++ lesson07/assignment/assignment-7/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Gallery from './GalleryView.vue'
+import Home from './MyHome.vue'
 
-Vue.config.productionTip = false
+// Add the Router Plugin
+Vue.use(VueRouter)
+
+// Define the routes
+const routes = [
+  { path: '/gallery', name: 'GalleryView', component: Gallery },
+  { path: '/', name: 'MyHome', component: Home }
+]
+
+// Create the router instance
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+const app = new Vue(App)
+
+app.$mount('#app')
+
+Vue.config.productionTip = false
