37451adef2fa85931fc57e37bc4dc13ffb219084
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-10-05 18:51:18 -0700

    Initial file updates

---
+++ lesson07/assignment/lesson-7/src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import GalleryView from './components/pages/GalleryView.vue'
+import MyHome from './components/pages/MyHome.vue'
+
+// Add the Router Plugin
+Vue.use(VueRouter)
+
+
+// Define the routes
+const routes = [
+  { path: '/gallery', name: 'GalleryView', component: GalleryView },
+  { path: '/', name: 'MyHome', component: MyHome }
+]
+
+// Create the router instance and pass the 'routes' option
+const router = new VueRouter({
+   routes,
+   mode: 'history'
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
+const app = new Vue(App)
+
+app.$mount('#app')
+
+Vue.config.productionTip = false
