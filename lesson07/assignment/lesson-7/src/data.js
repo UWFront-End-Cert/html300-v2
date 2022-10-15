2e5fc9c7bb4267f1cbabd4a0d743a715cfeb3a7e
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-10-15 00:20:32 -0700

    Removed practice portfolio files

--- lesson07/assignment/lesson-7/src/data.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-  image: [
-    { id: 1, name: "Image010", image: "./assets/G-Croom-WWII-010-th.jpg",   description: "A photo" },
-    { id: 2, name: "Image011", image: "./assets/G-Croom-WWII-011-th.jpg", description: "Another photo" },
-    { id: 3, name: "Image006", image: "./assets/G-Croom-WWII-006-th.jpg", description: "A third photo" }
-    ],
-    images: []}
-  ]
-}
