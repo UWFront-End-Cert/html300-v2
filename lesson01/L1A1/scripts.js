27fa7e55f26d1e0e677e8385efd1e7c543dd39ff
Emmett Keeton <emmaleek9@yahoo.com>
2022-12-20 10:08:04 -0800

    Worked on lesson 1 assignment 1

---
+++ lesson01/L1A1/scripts.js
@@ -0,0 +1,10 @@
+// set the data and variables
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+input = $('.input'),
+submit = $('.submit'),
+result = $('.result');
+
+const form1 = document.querySelector('#form1');
+form1.addEventListener('submit', function(event){
+  event.preventDefault();
+})
