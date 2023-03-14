d568f1decc58aa8989aee3ef014a22e92d4e811e
= <=>
2023-03-13 20:11:12 -0700

    Set up lesson 7 assignment

---
+++ lesson07/assignment/lesson-07/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).mount('#app')
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
