5250f7ee0ab96062b9b7d18ee90ba9d28ace42b8
Poipounder81 <103324222+Poipounder81@users.noreply.github.com>
2023-04-28 22:21:19 -0700

    lesson03 code-along activity

--- lesson03/activity/cars.js
+++ lesson03/activity/cars.js
@@ -1,3 +1,31 @@
 // TODO: Create HTML code from cars JSON
-
 const carsJSON = '[{"make":"Ford","model":"Mustang","year":2010,"color":"black"},{"make":"Chevy","model":"Corvette","year":1984,"color":"red"},{"make":"Jeep","model":"Wrangler","year":1999,"color":"silver"}]'
+
+const cars = JSON.parse(carsJSON)
+
+const container = document.querySelector('#list-container')
+
+cars.forEach(function(el){
+    let list = document.createElement('ul')
+    list.innerHTML = `
+    <li>${el.make} ${el.model}</li>
+    <li>Color: ${el.color}</li>
+    <li>Year: ${el.year}</li>
+    `
+    container.append(list)
+})
+
+
+
+// let carsHTML = cars.map(function(el) {
+//     let car =`<article class="car">
+//       <ul>
+//        <li class="car__make">${el.make}</li>
+//        <li class="car__model">${el.model}</li>
+//        <li class="car__details">Color: ${el.color}</li>
+//        <li class="car__details">Year: ${el.year}</li>
+//       </ul>
+//      </article>`;
+   
+//    return car;
+//    })
--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,5 +1,33 @@
 // Create the Dog class constructor
+class Dog {
+    //Give it expected parameters
+    constructor(name, age, breed, color) {
+        //Set those equal to the instance
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+        this.energyLevel = 0;
+        this.barkLevel = 10;
+    }
+
+    // pat method
+    pat () {
+        //Increment energy
+        this.energyLevel++
+        //Decrement bark
+        this.barkLevel--;
+        //log out the dog's info
+        console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy is ${this.energyLevel}, and current bark level is ${this.barkLevel}.`);
+    }
+}
 
 // Create Marty
+const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
 
 // Pat him 5 times
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
