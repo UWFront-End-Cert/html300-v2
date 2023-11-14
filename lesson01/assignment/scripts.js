77e360162b84a4da4844231af088d2ba4658cd94
kurosawaftw7 <jdharman@gmail.com>
2023-11-13 20:39:22 -0800

    Update files

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -1,32 +1,53 @@
 // Code that requires the DOM be loaded should not be run until the DOM is loaded
-document. addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
-  // Find the example form, and handle its submit event
   const exampleForm = document.querySelector('#example-form')
 
   exampleForm.addEventListener('submit', function(event){
-    // Prevent the form from trying to submit to a server
     event.preventDefault()
 
-    // An array that we can transform based on user input
+
     let data = [0, 1, 2, 3, 5, 8]
 
     const inputField = document.querySelector('#example-form .input')
     const userInput = inputField.value
     const userNumber = parseInt(userInput)
 
-    // If the user's input was a number, push it into the array and continue
     if(!isNaN(userNumber)){
       data.push(userNumber)
 
-      // Use reduce to sum all the numbers in the data array
       const sum = data.reduce((accumulator, currentElement) => accumulator + currentElement);
 
-      // Output the total to the page- no need to convert the numeric sum back to a string, HTML will just print the number as a string
       document.querySelector('#example-form .output').innerHTML = sum
     }
 
-    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  const exampleForm = document.querySelector('#example-form2')
+
+  exampleForm.addEventListener('submit', function(event){
+    event.preventDefault()
+
+
+    let data = [2, 1, 7, 3, 5, 1]
+
+    const inputField = document.querySelector('#example-form2 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    if(!isNaN(userNumber)){
+      data.push(userNumber)
+
+      const sum = data.reduce((accumulator, currentElement) => accumulator + currentElement);
+
+      document.querySelector('#example-form2 .output').innerHTML = sum
+    }
+
     inputField.value = ''
   })
 
