d6d84188087f27dc941fef9820f246c882e13380
kurosawaftw7 <jdharman@gmail.com>
2023-12-09 14:44:55 -0800

    Update files

--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,5 +1,34 @@
 // Create the Dog class constructor
 
+
+
 // Create Marty
 
 // Pat him 5 times
+
+class Dog {
+    constructor(name, age, breed, color) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+        this.energylevel = 0;
+        this.barklevel = 10;
+    }
+
+
+pat() {
+    this.energylevel++
+    this.barkLevel--;
+    console.log(`This dog's name is ${this.name}, age is 5{this.age}, current energy ${this.energylevel} and current bark level is ${this.barklevel}.`);
+    }
+}
+
+const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
+
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
+
