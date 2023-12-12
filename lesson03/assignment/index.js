40f284d2507054d097a6e38c6ea1bb26ab98c2bf
kurosawaftw7 <jdharman@gmail.com>
2023-12-11 19:55:56 -0800

    Update files

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,6 +1,12 @@
 
-
-let name = dev.name;
-let jobTitle = dev.jobTitle;
-let company = dev.company;
-let experience = dev.experience;
+function dev(name, jobTitle, company, experience, school, major, email, linkedInUrl, codeLanguages) {
+    let name = dev.name;
+    let jobTitle = dev.jobTitle;
+    let company = dev.company;
+    let experience = dev.experience;
+    let school = dev.school;
+    let major = dev.major;
+    let email = dev.email;
+    let linkedInUrl = dev.linkedInUrl;
+    let codeLanguages = dev.codeLanguages;
+}
